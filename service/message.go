a3373a07197d8afd7c337ee80d2cdabc46d63d0f
mengqingchun.m <mengqingchun.m@bytedance.com>
2024-06-01 23:43:40 +0800

    tst

--- service/message.go
+++ service/message.go
@@ -2,90 +2,71 @@ package service
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
+	"encoding/xml"
 	"github.com/baidubce/bce-qianfan-sdk/go/qianfan"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 // MessageHandler
 func MessageHandler(w http.ResponseWriter, r *http.Request) {
-	res := &JsonResult{}
 
-	w.Header().Set("content-type", "application/json")
+	bs, _ := io.ReadAll(r.Body)
+	msg := NewMsg(bs)
 
-	body, err := PeekRequest(r)
-	if err != nil {
-		res.Code = -1
-		res.ErrorMsg = err.Error()
-		msg, err := json.Marshal(res)
-		log.Printf("err: %s", err.Error())
-		if err != nil {
-			fmt.Fprint(w, "内部错误")
-		}
-		w.Write(msg)
-		return
-	}
-
-	m := &Model{}
-	if err := json.Unmarshal(body, &m); err != nil {
-		res.Code = -1
-		res.ErrorMsg = err.Error()
-		msg, err := json.Marshal(res)
-		log.Printf("err: %s", err.Error())
-		if err != nil {
-			fmt.Fprint(w, "内部错误")
-		}
-		w.Write(msg)
-		return
-	}
-	log.Printf("log body : %s", string(body))
+	log.Printf("log body : %s", msg.Content)
 
 	// 发起对话，例如介绍下北京
 	resp, err := chatClient.Do(
 		context.TODO(),
 		&qianfan.ChatCompletionRequest{
 			Messages: []qianfan.ChatCompletionMessage{
-				qianfan.ChatCompletionUserMessage(m.Content),
+				qianfan.ChatCompletionUserMessage(msg.Content),
 			},
 		},
 	)
 	if err != nil {
-		res.Code = -1
-		res.ErrorMsg = err.Error()
-		log.Printf("err: %s", err.Error())
-		msg, err := json.Marshal(res)
-		if err != nil {
-			fmt.Fprint(w, "内部错误")
-		}
-		w.Write(msg)
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Write(msg.GenerateEchoData(err.Error()))
 		return
 	}
 
 	log.Printf("qianfan body : %s", resp.Result)
-	w.Header().Set("content-type", "application/json")
-	w.Write([]byte(resp.Result))
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(msg.GenerateEchoData(resp.Result))
 }
 
-func PeekRequest(request *http.Request) ([]byte, error) {
-	if request.Body != nil {
-		byts, err := io.ReadAll(request.Body) // io.ReadAll as Go 1.16, below please use ioutil.ReadAll
-		if err != nil {
-			return nil, err
-		}
-		return byts, nil
-	}
-
-	return make([]byte, 0), nil
-}
-
-type Model struct {
-	MsgId        int64  `json:"MsgId"`
-	Content      string `json:"Content"`
-	MsgType      string `json:"MsgType"`
-	CreateTime   int64  `json:"CreateTime"`
-	FromUserName string `json:"FromUserName"`
+type Msg struct {
 	ToUserName   string `json:"ToUserName"`
+	FromUserName string `json:"FromUserName"`
+	CreateTime   int64  `json:"CreateTime"`
+	MsgType      string `json:"MsgType"`
+	Event        string `json:"Event"`
+	Content      string `json:"Content"`
+	Recognition  string `json:"Recognition"`
+
+	MsgId int64 `xml:"MsgId,omitempty"`
+}
+
+func NewMsg(data []byte) *Msg {
+	var msg Msg
+	if err := xml.Unmarshal(data, &msg); err != nil {
+		return nil
+	}
+	return &msg
+}
+
+func (msg *Msg) GenerateEchoData(s string) []byte {
+	data := Msg{
+		ToUserName:   msg.FromUserName,
+		FromUserName: msg.ToUserName,
+		CreateTime:   time.Now().Unix(),
+		MsgType:      "text",
+		Content:      s,
+	}
+	bs, _ := xml.Marshal(&data)
+	return bs
 }
